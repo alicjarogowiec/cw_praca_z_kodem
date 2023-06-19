28f22f16c28c092d4c7db641965d1d03a58d72d0
alicjarogowiec <rogowiec.alicja@gmail.com>
2023-06-19 15:12:47 +0200

    Add module and function docstrings

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+"""Module containing function greeting WSB"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Function printing a greeting"""
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 if __name__ == "__main__":
